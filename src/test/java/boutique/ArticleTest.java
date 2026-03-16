8298b56e1b2eb0a6dfe6d9fbeceedb4766f4bc18
Jonas-Mukuri-Maka <zmukuri@gmail.com>
2026-03-16 14:48:22 +0100

    Ajoute SuiteBoutique / Modification d'arborscence de tests

--- src/test/java/boutique/ArticleTest.java
+++ src/test/java/boutique/ArticleTest.java
@@ -1,3 +1,5 @@
+package boutique;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
--- src/test/java/boutique/PanierReductionTest.java
+++ src/test/java/boutique/PanierReductionTest.java
@@ -1,3 +1,4 @@
+package boutique;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.params.ParameterizedTest;
